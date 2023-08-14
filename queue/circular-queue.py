3bcfc0f1aa1e3ebaaf5f0d48fc91b60a8e3b2581
Vitor Mateus <vitor7jan@gmail.com>
2023-08-14 16:50:53 -0300

    added circular queue

--- queue/circular-queue.py
+++ queue/circular-queue.py
@@ -4,14 +4,13 @@ class Node():
     __data__ = None
     __nextNode__ = None
 
-    def __init__(self, data, nextNode):
+    def __init__(self, data):
         self.__data__ = data
-        self.setNext(nextNode)
 
     def setNext(self, nextNode):
         self.__nextNode__ = nextNode
 
-    def getNextNode(self):
+    def getNext(self):
         return self.__nextNode__
     
     def getData(self):
@@ -44,32 +43,53 @@ class Queue():
     def __decrementNodes__(self):
         self.__currQttNodes__ -= 1
 
+    def __isFull__(self)->bool:
+        return self.__currQttNodes__ == self.__maxNodes__
+
+    def isEmpty(self)->bool:
+        return self.__currQttNodes__ == 0
+
     def put(self, data):
-        assert self.__currQttNodes__ < self.__maxNodes__, "queue is full"
-        newNode = Node(data=data, nextNode=None)
+        newNode = Node(data)
         if self.__currQttNodes__ == 0:
+            newNode.setNext(newNode)
             self.__setFirst__(newNode)
             self.__setLast__(newNode)
+            self.__incrementNodes__()
+            return
+        self.__getLast__().setNext(newNode)
+        self.__setLast__(newNode)
+        if self.__isFull__():
+            secondNode = self.__getFirst__().getNext()
+            newNode.setNext(secondNode)
+            self.__setFirst__(secondNode)
         else:
-            self.__getLast__().setNext(newNode)
-            self.__setLast__(newNode)
-        self.__incrementNodes__()
+            firstNode = self.__getFirst__()
+            newNode.setNext(firstNode)
+            self.__incrementNodes__()
 
     def get(self) -> Node:
+        assert self.__currQttNodes__ > 0, "queue is empty"
         firstNode = self.__getFirst__()
-        if firstNode == None: return None
-        secondNode: Node = firstNode.getNextNode()
-        self.__setFirst__(secondNode)
-        if secondNode == None: self.__setLast__(None)
         self.__decrementNodes__()
+        if self.__currQttNodes__ == 0:
+            self.__setFirst__(None)
+            self.__setLast__(None)
+            return firstNode
+        secondNode: Node = firstNode.getNext()
+        self.__getLast__().setNext(secondNode)
+        self.__setFirst__(secondNode)
         return firstNode
     
     def print(self):
         msg = ""
-        currentNode = self.__getFirst__()
-        while not currentNode == None:
+        loop  = True
+        if self.isEmpty(): loop = False
+        firstNode = currentNode = self.__getFirst__()
+        while loop:
             msg += "{0} ".format(currentNode.getData())
-            currentNode = currentNode.getNextNode()
+            currentNode = currentNode.getNext()
+            if currentNode == firstNode: break
         print("queue: {0}, total nodes: {1}".format(msg, self.__currQttNodes__))
 
     def clear(self):
@@ -79,17 +99,15 @@ class Queue():
 
 def main():
     maxNodes = 3
-    queue = Queue(maxNodes=maxNodes)
-    print(queue.get())
+    queue: Queue = Queue(maxNodes=maxNodes)
+    # queue.get()
     queue.print()
-    myRange = range(0, maxNodes)
-    for i in myRange:
+    for i in range(0, maxNodes + 2):
         queue.put(i)
         queue.print()
-    # queue.put(1)
     # queue.clear()
-    # queue.print()
-    for i in myRange:
+    queue.print()
+    while not queue.isEmpty():
         node = queue.get()
         if not node == None: print(node.getData())
         queue.print()
