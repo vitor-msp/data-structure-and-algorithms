a1f5d2cbd96afa7aa1a3d8e47861d9229b885cb7
Vitor Mateus <vitor7jan@gmail.com>
2023-08-16 13:57:22 -0300

    added selection sort

--- 10-ordination/insertion-sort.js
+++ 10-ordination/insertion-sort.js
@@ -8,11 +8,10 @@ const insertionSort = (list) => {
     }
     list[compareIndex + 1] = toInsert;
   }
-  return list;
 };
 
 (() => {
-  const input = [
+  const list = [
     100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82,
     81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 63,
     62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44,
@@ -20,6 +19,6 @@ const insertionSort = (list) => {
     24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5,
     4, 3, 2, 1,
   ];
-  const output = insertionSort(input);
-  console.log(output);
+  insertionSort(list);
+  console.log(list);
 })();
