c0ddf20e53d845cc6682b1d3a52204ba5dff1b9c
Vitor Mateus <vitor7jan@gmail.com>
2023-08-16 17:27:50 -0300

    added merge sort

--- 10-ordination/selection-sort.js
+++ 10-ordination/selection-sort.js
@@ -29,6 +29,8 @@ const selectionSort = (list) => {
     24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5,
     4, 3, 2, 1,
   ];
+  console.time();
   selectionSort(list);
+  console.timeEnd();
   console.log(list);
 })();
